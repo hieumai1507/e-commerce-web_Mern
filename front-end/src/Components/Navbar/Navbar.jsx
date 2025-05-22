82d7b910f15ce30335b707acd5509e1f45c85263
hieumai1507 <114709586+hieumai1507@users.noreply.github.com>
2025-05-22 11:59:44 +0700

    fix responsive UI

--- front-end/src/Components/Navbar/Navbar.jsx
+++ front-end/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
-import React, { useState, useContext } from 'react'
-import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
-import { Link } from 'react-router-dom';
-import { ShopContext } from '../../Context/ShopContext';
+import React, { useState, useContext } from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
+
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
-  const {getTotalCartItems} = useContext(ShopContext);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const { getTotalCartItems } = useContext(ShopContext);
+
+  const toggleMobileMenu = () => {
+    setMobileMenu(!mobileMenu);
+  };
 
   return (
     <div className="navbar">
@@ -15,22 +21,81 @@ const Navbar = () => {
         <p>SHOPPING</p>
       </div>
 
-      <ul className="nav-menu">
-        <li onClick={() => setMenu("shop")}><Link style={{ textDecoration: 'none'}} to="/">Shop</Link>{menu === "shop" ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu("mens")}><Link style={{ textDecoration: 'none'}} to="/mens">Men</Link>{menu === "mens" ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu("womens")}><Link style={{ textDecoration: 'none'}} to="/womens">Women</Link>{menu === "womens" ? <hr /> : <></>}</li>
-        <li onClick={() => setMenu("kids")}><Link style={{ textDecoration: 'none'}} to="/kids">Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
+      <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`nav-menu ${mobileMenu ? "active" : ""}`}>
+        <li
+          onClick={() => {
+            setMenu("shop");
+            setMobileMenu(false);
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/">
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+            setMobileMenu(false);
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/mens">
+            Men
+          </Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+            setMobileMenu(false);
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/womens">
+            Women
+          </Link>
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+            setMobileMenu(false);
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="/kids">
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
 
       <div className="nav-sign-in-cart">
-        {localStorage.getItem("auth-token")
-        ?<button onClick={()=> {localStorage.removeItem('auth-token'); window.location.replace('/')}}>Logout</button>
-        :<Link to='/signin'><button>Sign in</button></Link>}
-        <Link to="/cart"><img src={cart_icon} alt=""/></Link>
-        <div className='nav-cart-count'>{getTotalCartItems()}</div>
+        {localStorage.getItem("auth-token") ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("auth-token");
+              window.location.replace("/");
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <Link to="/signin">
+            <button>Sign in</button>
+          </Link>
+        )}
+        <Link to="/cart">
+          <img src={cart_icon} alt="" />
+        </Link>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
